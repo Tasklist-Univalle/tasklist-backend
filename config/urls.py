039fa5f1546edb046c85fc128d9eef361e48c3aa
Daniel <dani.media6@gmail.com>
2026-02-21 08:19:19 -0500

    fix: add root health response and remove turtle import

--- config/urls.py
+++ config/urls.py
@@ -14,10 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import include, path
 
+def home(request):
+    return JsonResponse({"status": "Backend running 🚀"})
+
 urlpatterns = [
+    path("", home),
     path('admin/', admin.site.urls),
      path("api/", include("tasks.urls")),
 ]
